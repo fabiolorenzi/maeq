f04e8c65640bec1f5730cfd07f55df2fbb6e57e3
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-08-01 13:52:01 +0100

    created enums for frequencies

---
+++ src/Frequencies.h
@@ -0,0 +1,17 @@
+#pragma once
+
+enum LowFreq
+{
+    Freq_32,
+    Freq_64,
+    Freq_128
+};
+
+enum HighFreq
+{
+    Freq_5800,
+    Freq_8192,
+    Freq_11600,
+    Freq_16400,
+    Freq_22100
+};
